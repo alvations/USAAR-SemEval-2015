fe497a811eec320b9a44271bd0e14adb66a69122
alvas <alvas@ubi>
2014-12-18 16:29:45 +0100

    added test data

--- task02-USAAR-SHEFFIELD/asiya.py
+++ task02-USAAR-SHEFFIELD/asiya.py
@@ -10,17 +10,24 @@ def get_asiya_scores():
     
     feature_data = {}
     for infile in os.listdir(indir):
-        if infile in ['features.cp', 'features.sr', 'features.ne']:
+        if infile in ['features.cp', 'features.sr', 'features.ne', 
+                      'features.esa']:
             continue
         data = [[float(i) for i in line.strip().split()] 
                 for line in io.open(indir+infile, 'r')]
         feature_data[infile] = data
     
-    seventy_seven = [i + j + k + l for i,j,k, l in 
+    _seventy_seven = [i + j + k  for i,j,k in 
                      zip(feature_data['features.meteor'], 
                          feature_data['features.sp'], 
-                         feature_data['features.ngram'], 
-                         feature_data['features.esa']) if len(i+j+k+l) == 77]
-    return seventy_seven
+                         feature_data['features.ngram'])]
+    seventy_seven = []
+    to_remove = []
 
-
+    for i,j in enumerate(_seventy_seven):
+        if len(j) != 76:
+            to_remove.append(i)
+        else:
+            seventy_seven.append(j)
+    
+    return seventy_seven, to_remove
--- task02-USAAR-SHEFFIELD/carolling.py
+++ task02-USAAR-SHEFFIELD/carolling.py
@@ -25,52 +25,68 @@ sts_scores = np.array(get_sts_scores('score.train'))
 test_meteor_scores = np.array(get_meteor_scores('meteor.output.test'))
 
 x = meteor_scores
-x = np.array(get_asiya_scores())
+_x, to_remove = get_asiya_scores()
+
+x = np.array(_x)
 y = sts_scores
-n = len(sts_scores)
-
-#xt = test_meteor_scores
+y =np.delete(y, to_remove, axis=0)
+n = len(y)
 xt = x
-np.savetxt('x', x)
 
-
+#print len(_x), len(x), len(y)
 
 # Linear Regression
 #print 'linear'
 lr = LinearRegression()
-lr.fit(x[:, np.newaxis], y)
-lr_sts_scores = lr.predict(xt[:, np.newaxis])
+#lr.fit(x[:, np.newaxis], y)
+#lr_sts_scores = lr.predict(xt[:, np.newaxis])
+lr.fit(x, y)
+lr_sts_scores = lr.predict(xt)
+
 
 # Baysian Ridge Regression
 #print 'baysian ridge'
 br = BayesianRidge(compute_score=True)
-br.fit(x[:, np.newaxis], y)
-br_sts_scores = br.predict(xt[:, np.newaxis])
+#br.fit(x[:, np.newaxis], y)
+#br_sts_scores = br.predict(xt[:, np.newaxis])
+br.fit(x, y)
+br_sts_scores = br.predict(xt)
+
 
 # Elastic Net
 #print 'elastic net'
 enr = ElasticNet()
-enr.fit(x[:, np.newaxis], y)
-enr_sts_scores = enr.predict(xt[:, np.newaxis])
+#enr.fit(x[:, np.newaxis], y)
+#enr_sts_scores = enr.predict(xt[:, np.newaxis])
+enr.fit(x, y)
+enr_sts_scores = enr.predict(xt)
 
 
 # Passive Aggressive Regression
 #print 'passive aggressive'
 par = PassiveAggressiveRegressor()
-par.fit(x[:, np.newaxis], y)
-par_sts_scores = par.predict(xt[:, np.newaxis])
+par.fit(x, y)
+par_sts_scores = par.predict(xt)
+#par.fit(x[:, np.newaxis], y)
+#par_sts_scores = par.predict(xt[:, np.newaxis])
 
 # RANSAC Regression
 #print 'ransac'
 ransac = RANSACRegressor()
-ransac.fit(x[:, np.newaxis], y)
-ransac_sts_scores = ransac.predict(xt[:, np.newaxis])
+#ransac.fit(x[:, np.newaxis], y)
+#ransac_sts_scores = ransac.predict(xt[:, np.newaxis])
+ransac.fit(x, y)
+ransac_sts_scores = ransac.predict(xt)
+
 
 # Logistic Regression
 #print 'logistic'
 lgr = LogisticRegression()
-lgr.fit(x[:, np.newaxis], y)
-lgr_sts_scores = lgr.predict(xt[:, np.newaxis])
+#lgr.fit(x[:, np.newaxis], y)
+#lgr_sts_scores = lgr.predict(xt[:, np.newaxis])
+lgr.fit(x, y)
+lgr_sts_scores = lgr.predict(xt)
+
 
 '''
 # SLOW Regressors !!!!
@@ -83,6 +99,7 @@ ard.fit(x[:, np.newaxis], y)
 ## SLOW Regressors !!!!
 '''
 
+'''
 # Isotonic Regression
 #print 'isotonic'
 ir = IsotonicRegression()
@@ -97,7 +114,7 @@ params = {'n_estimators': 500, 'max_depth': 4, 'min_samples_split': 1,
 gbr = ensemble.GradientBoostingRegressor(**params)
 gbr.fit(x[:, np.newaxis], y)
 gbr_sts_scores = gbr.predict(xt[:, np.newaxis])
-
+'''
 
 '''
 # Decision Tree Regression
@@ -114,17 +131,26 @@ dtr5_sts_scores = dtr5.predict(xt[:, np.newaxis])
 # Support Vector Regressions
 #print 'support vector'
 svr_lin = SVR(kernel='linear', C=1e3)
-y_lin = svr_lin.fit(x[:,np.newaxis], y)  
-y_lin_sts_scores = y_lin.predict(xt[:, np.newaxis])
+y_lin = svr_lin.fit(x, y)  
+y_lin_sts_scores = y_lin.predict(xt)
+#y_lin = svr_lin.fit(x[:,np.newaxis], y)  
+#y_lin_sts_scores = y_lin.predict(xt[:, np.newaxis])
+
 
 svr_poly = SVR(kernel='poly', C=1e3, degree=2)
-y_poly = svr_poly.fit(x[:,np.newaxis], y)
-y_poly_sts_scores = y_poly.predict(xt[:, np.newaxis])
+y_poly = svr_poly.fit(x, y)
+y_poly_sts_scores = y_poly.predict(xt)
+#y_poly = svr_poly.fit(x[:,np.newaxis], y)
+#y_poly_sts_scores = y_poly.predict(xt[:, np.newaxis])
+
 
 # Gives runtime warning.
 svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
-y_rbf = svr_rbf.fit(x[:,np.newaxis], y)  
-y_rbf_sts_scores = y_rbf.predict(xt[:, np.newaxis])
+#y_rbf = svr_rbf.fit(x[:,np.newaxis], y)  
+#y_rbf_sts_scores = y_rbf.predict(xt[:, np.newaxis])
+y_rbf = svr_rbf.fit(x, y)  
+y_rbf_sts_scores = y_rbf.predict(xt)
+
 
 scores = np.array(zip(lr_sts_scores,
 br_sts_scores,
